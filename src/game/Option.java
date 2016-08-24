a9364a5c86924212004dadde7990e38305f242d8
sdarekar <sdarekar@admin-PC>
2016-08-24 11:18:59 +0530

    first commit

---
+++ src/game/Option.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Option {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("your game is hocky");
+	}
+
+}
---
+++ src/game/Players.java
@@ -0,0 +1,10 @@
+package game;
+
+public class Players {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Toatl no of players are 9");
+	}
+
+}
